20b724d8dadbc96a56cc790ee6261653bf0c2c83
julienvvtv <40123287+julienvvtv@users.noreply.github.com>
2019-05-16 09:21:12 +0200

    Update i2c_master_TB2.vhd

--- I2C_driver/i2c_master_TB2.vhd
+++ I2C_driver/i2c_master_TB2.vhd
@@ -1,30 +1,3 @@
-
---------------------------------------------------------------------------------
---
---   FileName:         i2c_master.vhd
---   Dependencies:     none
---   Design Software:  Quartus II 32-bit Version 11.1 Build 173 SJ Full Version
---
---   HDL CODE IS PROVIDED "AS IS."  DIGI-KEY EXPRESSLY DISCLAIMS ANY
---   WARRANTY OF ANY KIND, WHETHER EXPRESS OR IMPLIED, INCLUDING BUT NOT
---   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
---   PARTICULAR PURPOSE, OR NON-INFRINGEMENT. IN NO EVENT SHALL DIGI-KEY
---   BE LIABLE FOR ANY INCIDENTAL, SPECIAL, INDIRECT OR CONSEQUENTIAL
---   DAMAGES, LOST PROFITS OR LOST DATA, HARM TO YOUR EQUIPMENT, COST OF
---   PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY OR SERVICES, ANY CLAIMS
---   BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF),
---   ANY CLAIMS FOR INDEMNITY OR CONTRIBUTION, OR OTHER SIMILAR COSTS.
---
---   Version History
---   Version 1.0 11/1/2012 Scott Larson
---     Initial Public Release
---   Version 2.0 06/20/2014 Scott Larson
---     Added ability to interface with different slaves in the same transaction
---     Corrected ack_error bug where ack_error went 'Z' instead of '1' on error
---     Corrected timing of when ack_error 	 clears
---    
---------------------------------------------------------------------------------
-
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
@@ -33,7 +6,6 @@ ENTITY i2c_master_TB2 IS
 END i2c_master_TB2;
 
 ARCHITECTURE beh OF i2c_master_TB2 IS
-	-- test 	s
 		signal clk       :    STD_LOGIC :='0';
 		signal reset_n   :    STD_LOGIC :='1';
 		signal ena       :    STD_LOGIC;                    
@@ -46,7 +18,7 @@ ARCHITECTURE beh OF i2c_master_TB2 IS
 		signal sda       :    STD_LOGIC;                    
 		signal scl       :    STD_LOGIC;
 		signal sReady	  :  STD_LOGIC;
-	    signal sStart		:  STD_LOGIC;
+		 signal sStart		:  STD_LOGIC;
 		 signal sSlv_ack1		:  STD_LOGIC;					
 		 signal sSlv_ack2		:  STD_LOGIC;				
 		 signal sMstr_ack		:  STD_LOGIC;
@@ -56,33 +28,32 @@ ARCHITECTURE beh OF i2c_master_TB2 IS
   -- clock period for simulation
   constant clk_period : time := 10 ns;
 	
-	component i2c_master IS
-	  GENERIC(
-		 input_clk :  INTEGER := 50_000_000; 
-		 bus_clk   :  INTEGER := 400_000);   
-	  PORT(
-		 clk       :  IN      STD_LOGIC;                    
-		 reset_n   :  IN      STD_LOGIC;                    
-		 ena       :  IN      STD_LOGIC;                    
-		 addr      :  IN      STD_LOGIC_VECTOR(6 DOWNTO 0); 
-		 rw        :  IN      STD_LOGIC;                    
-		 data_wr   :  IN      STD_LOGIC_VECTOR(7 DOWNTO 0); 
-		 busy      :  OUT     STD_LOGIC;                    
-		 data_rd   :  OUT     STD_LOGIC_VECTOR(7 DOWNTO 0); 
-		 ack_error :  BUFFER  STD_LOGIC;                    
-		 oReady	  :  out      STD_LOGIC;
-	    oStart		:  out      STD_LOGIC;
-		 oSlv_ack1		:  out      STD_LOGIC;					
-		 oSlv_ack2		:  out      STD_LOGIC;					
-		 oMstr_ack		:  out      STD_LOGIC;
-		 oStop		:  out      STD_LOGIC;
-		 oRd			: out 		STD_LOGIC;
-		 sda       :  INOUT   STD_LOGIC;                    
-		 scl       :  INOUT   STD_LOGIC);                   
-	END component;
+component i2c_master IS
+  GENERIC(
+	 input_clk :  INTEGER := 50_000_000; 
+	 bus_clk   :  INTEGER := 400_000);   
+  PORT(
+	 clk       :  IN      STD_LOGIC;                    
+	 reset_n   :  IN      STD_LOGIC;                    
+	 ena       :  IN      STD_LOGIC;                    
+	 addr      :  IN      STD_LOGIC_VECTOR(6 DOWNTO 0); 
+	 rw        :  IN      STD_LOGIC;                    
+	 data_wr   :  IN      STD_LOGIC_VECTOR(7 DOWNTO 0); 
+	 busy      :  OUT     STD_LOGIC;                    
+	 data_rd   :  OUT     STD_LOGIC_VECTOR(7 DOWNTO 0); 
+	 ack_error :  BUFFER  STD_LOGIC;                    
+	 oReady	  :  out      STD_LOGIC;
+    oStart		:  out      STD_LOGIC;
+	 oSlv_ack1		:  out      STD_LOGIC;					
+	 oSlv_ack2		:  out      STD_LOGIC;					
+	 oMstr_ack		:  out      STD_LOGIC;
+	 oStop		:  out      STD_LOGIC;
+	 oRd			: out 		STD_LOGIC;
+	 sda       :  INOUT   STD_LOGIC;                    
+	 scl       :  INOUT   STD_LOGIC);                   
+END component;
 
 BEGIN
-
 	DUT : entity work.i2c_master(logic) 
 		 port map( clk=> clk, reset_n => reset_n, ena => ena, addr => addr , rw => rw , data_wr => data_wr , busy => busy , data_rd => data_rd ,ack_error => ack_error,
 		  oReady=>sReady ,
@@ -117,7 +88,6 @@ BEGIN
 			'0' when sSlv_ack2 ='1' else
 			'0' when sMstr_ack ='1' else 'Z';
 	
-	
 	pRESETn: process
 	begin
 		reset_n <= '1';
@@ -127,11 +97,10 @@ BEGIN
 		reset_n <= '1';
 		wait;
 	end process;
-		
-		
-			temp : process
+
+	temp : process
 	begin
-		addr<="1001000"; 					--
+		addr<="1001000"; 						--
 		rw<='0';
 		data_wr<="00000001";	
 		ena<='1';							--réglage de la résolution
@@ -153,6 +122,4 @@ BEGIN
 		wait until sStop='1';
 	end process;
 		
-		
-		
-		END beh;
+END beh;
